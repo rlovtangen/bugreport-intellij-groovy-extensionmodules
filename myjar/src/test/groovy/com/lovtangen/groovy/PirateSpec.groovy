1eed5db923e1beca125b78415435549c5f0e22bc
Ronny Løvtangen <ronny@lovtangen.com>
2016-12-06 08:26:17 +0100

    Add test where extension method is invoked as static method rather than instance method.

--- myjar/src/test/groovy/com/lovtangen/groovy/PirateSpec.groovy
+++ myjar/src/test/groovy/com/lovtangen/groovy/PirateSpec.groovy
@@ -12,4 +12,12 @@ class PirateSpec extends Specification {
         originalText.likeAPirate() == "Yo-ho-ho, Groovy be th' greatest language o' th' JVM."
     }
  
+    def "likeAPirate method should work when invoked as static method"() {
+        given:
+        final String originalText = "Hi, Groovy is the greatest language of the JVM."
+
+        expect:
+        PirateExtension.likeAPirate(originalText) == "Yo-ho-ho, Groovy be th' greatest language o' th' JVM."
+    }
+
 }
